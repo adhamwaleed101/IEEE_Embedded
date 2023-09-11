9783cfdd85fa151b8032f79977d9f0485914dab9
Adham Waleed <68808950+adhamwaleed101@users.noreply.github.com>
2023-09-11 23:10:35 +0300

    Add files via upload

---
+++ COTS/STM32F103C8/MCAL/RCC/RCC_program.c
@@ -0,0 +1,200 @@
+/*****************************< LIB *****************************/
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+/*****************************< MCAL *****************************/
+#include "RCC_interface.h"
+#include "RCC_private.h"
+#include "RCC_config.h"
+
+/*****************************< Function Implementations *****************************/
+Std_ReturnType MCAL_RCC_InitSysClock(void)
+{
+    Std_ReturnType Local_FunctionStatus = E_NOT_OK;
+
+    #if RCC_SYSCLK == RCC_HSE
+        
+        /**< Enable the external clock to be the source for the system clock. */
+        #if RCC_CLK_BYPASS == RCC_RC_CLK_
+            SET_BIT(RCC_CR, RCC_CR_HSEBYP); /**< Choose RC as a SYSCLK */
+        #elif RCC_CLK_BYPASS == RCC_CRYSTAL_CLK_
+            CLR_BIT(RCC_CR, RCC_CR_HSEBYP); /**< Choose CRYSTAL as a SYSCLK */
+        #else 
+            #error "Wrong Choice !!"
+        #endif /**< RCC_CLK_BYPASS */
+
+        /**< Enable the High-Speed External clock. */
+        SET_BIT(RCC_CR, RCC_CR_HSEON);
+
+        /**< Wait until the High-Speed External clock is stable. */
+        while(!GET_BIT(RCC_CR, RCC_CR_HSERDY));
+
+        /**< Select High-Speed External clock as the system clock source. */
+        RCC_CFGR = 0x00000001;
+
+        Local_FunctionStatus = E_OK;
+
+    #elif RCC_SYSCLK == RCC_HSI
+        
+        /**< Enable the High-Speed Internal clock. */
+        SET_BIT(RCC_CR, RCC_CR_HSION);
+
+        /**< Wait until the High-Speed Internal clock is stable. */
+        while(!GET_BIT(RCC_CR, RCC_CR_HSIRDY));
+
+        /**< Select High-Speed Internal clock as the system clock source. */
+        RCC_CFGR = 0x00000000;
+
+        Local_FunctionStatus = E_OK;
+
+    #elif RCC_SYSCLK == RCC_PLL
+
+    
+
+    #else
+        #error "Wrong Choice !!"
+
+    #endif /**< RCC_SYSCLK */
+
+    return Local_FunctionStatus;
+}
+
+Std_ReturnType MCAL_RCC_EnablePeripheral(u8 Copy_BusId, u8 Copy_PeripheralId)
+{
+    Std_ReturnType Local_FunctionStatus = E_NOT_OK;
+
+    switch(Copy_BusId)
+    {
+        /**< Enable the peripheral on the AHB bus. */
+        case RCC_AHB:
+            SET_BIT(RCC_AHBENR, Copy_PeripheralId);
+            Local_FunctionStatus = E_OK;
+            break;
+
+        /**< Enable the peripheral on the APB1 bus. */
+        case RCC_APB1:
+            SET_BIT(RCC_APB1ENR, Copy_PeripheralId);
+            Local_FunctionStatus = E_OK;
+            break;
+
+        /**< Enable the peripheral on the APB2 bus. */
+        case RCC_APB2:
+            SET_BIT(RCC_APB2ENR, Copy_PeripheralId);
+            Local_FunctionStatus = E_OK;
+            break;
+
+        default:
+            Local_FunctionStatus = E_NOT_OK;
+            break;
+    }
+
+    return Local_FunctionStatus;
+}
+
+Std_ReturnType MCAL_RCC_DisablePeripheral(u8 Copy_BusId, u8 Copy_PeripheralId)
+{
+    Std_ReturnType Local_FunctionStatus = E_NOT_OK;
+
+    switch(Copy_BusId)
+    {
+        /**< Disable the peripheral on the AHB bus. */
+        case RCC_AHB:
+            CLR_BIT(RCC_AHBENR, Copy_PeripheralId);
+            Local_FunctionStatus = E_OK;
+            break;
+
+        /**< Disable the peripheral on the APB1 bus. */
+        case RCC_APB1:
+            CLR_BIT(RCC_APB1ENR, Copy_PeripheralId);
+            Local_FunctionStatus = E_OK;
+            break;
+
+        /**< Disable the peripheral on the APB2 bus. */
+        case RCC_APB2:
+            CLR_BIT(RCC_APB2ENR, Copy_PeripheralId);
+            Local_FunctionStatus = E_OK;
+            break;
+
+        default:
+            Local_FunctionStatus = E_NOT_OK;
+            break;
+    }
+
+    return Local_FunctionStatus;
+}
+Std_ReturnType MCAL_RCC_SetPLL(u8 input_clock,u8 multiplier)
+{
+    Std_ReturnType Local_FunctionStatus = E_NOT_OK;
+    if (multiplier > 10){
+        #error "invalid multiplier"
+        return Local_FunctionStatus
+
+    }
+    if (RCC_SYSCLK * multiplier > 72000000){
+        #error "invalid multiplier, exceed 72mHz"
+        return Local_FunctionStatus
+
+    }
+
+
+
+    switch(multiplier)
+    {
+    
+        case 4:
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_1);
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_2);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_3);
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_4);
+            Local_FunctionStatus = E_OK;
+            break;
+
+         case 5:
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_1);
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_2);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_3);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_4);
+            Local_FunctionStatus = E_OK;
+            break;
+         case 6:
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_1);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_2);
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_3);
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_4);
+            Local_FunctionStatus = E_OK;
+            break;
+         case 7:
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_1);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_2);
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_3);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_4);
+            Local_FunctionStatus = E_OK;
+            break;
+         case 8:
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_1);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_2);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_3);
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_4);
+            Local_FunctionStatus = E_OK;
+            break;
+        case 9:
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_1);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_2);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_3);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_4);
+            Local_FunctionStatus = E_OK;
+            break;
+        case 10: /*Multiplies by 6.5 written as 10 to keep integer form*/
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_1);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_2);
+            CLR_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_3);
+            SET_BIT(RCC_CFGR, RCC_CFGR_PLLMUL_4);
+            Local_FunctionStatus = E_OK;
+            break;
+
+
+
+     
+    }
+
+    return Local_FunctionStatus;
+}
